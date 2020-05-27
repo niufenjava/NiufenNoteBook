85f3b571a8de3743aba3dddeb6d3aebfd21de433
haijun.zhang <haijun.zhang@luckincoffee.com>
2020-05-27 14:22:40 +0800

    字符串分割器工具类

--- NiufenCommon/src/main/java/io/niufen/common/text/StringSpliter.java
+++ NiufenCommon/src/main/java/io/niufen/common/text/StringSpliter.java
@@ -31,7 +31,8 @@ public class StringSpliter {
      */
     private static final int NONE_LIMIT_INT = IntConstants.ONE_MINUS;
 
-    //-------- Split by Char ---------
+
+    //-------- Split System FilePath ---------
 
     /**
      * 切分字符串路径，根据当前系统的默认路径进行切分
@@ -40,58 +41,93 @@ public class StringSpliter {
      * @return 切分后的集合
      */
     public static List<String> splitPath(String path) {
-        return splitPath(path, IntConstants.ZERO);
+        return splitPath(path, NONE_LIMIT_INT);
     }
 
     /**
-     * 切分字符串路径，根据当前系统的默认路径进行切分
-     * 返回字符串数组
-     *
-     * @param path 系统文件或文件夹路径
-     * @return 切分后的字符串数组
-     */
-    public static String[] splitPathToArray(String path) {
-        return toArray(splitPath(path));
-    }
-
-    /**
-     * 切分字符串路径，根据当前系统的默认路径进行切分
+     * 切分字符串路径，根据当前系统的默认路径进行切分。limit 分片限制
      *
      * @param path  系统文件或文件夹路径
-     * @param limit 限制分片数
+     * @param limit 限制分片数，返回集合的大小
      * @return 切分后的集合
      */
     public static List<String> splitPath(String path, int limit) {
         return split(path, SYSTEM_FILE_SEPARATOR, limit, Boolean.TRUE, Boolean.TRUE);
     }
 
-    /**
-     * 切分字符串路径，根据当前系统的默认路径进行切分
-     * 返回字符串数组
-     *
-     * @param path  系统文件或文件夹路径
-     * @param limit 限制分片数
-     * @return 切分后的字符串数组
-     */
-    public static String[] splitPathToArray(String path, int limit) {
-        return toArray(splitPath(path, limit));
-    }
+
+    //-------- Split by Char 根据字符对字符串进行切割 ---------
 
     /**
-     * 切分字符串
+     * 根据给定的字符，切分字符串
      *
-     * @param str         被切分的字符串
-     * @param separator   分隔符字符
-     * @param ignoreEmpty 是否忽略空串
+     * @param str       被切分的字符串
+     * @param separator 分隔符字符
      * @return 切分后的集合
      */
-    public static List<String> splitTrim(String str, char separator, boolean ignoreEmpty) {
-        return split(str, separator, IntConstants.ZERO, Boolean.TRUE, ignoreEmpty);
+    public static List<String> split(String str, char separator) {
+        return split(str, separator, NONE_LIMIT_INT, Boolean.FALSE, Boolean.FALSE, Boolean.FALSE);
+    }
+
+    /**
+     * 根据给定的字符，切分字符串，限制分片
+     *
+     * @param str       被切分的字符串
+     * @param separator 分隔符字符
+     * @param limit     分片限制数
+     * @return 切分后的集合
+     */
+    public static List<String> splitLimit(String str, char separator, int limit) {
+        return split(str, separator, limit, Boolean.FALSE, Boolean.FALSE);
+    }
+
+    /**
+     * 切分字符串，大小写敏感，去除每个元素两边空白符
+     *
+     * @param str       被切分的字符串
+     * @param separator 分隔符字符
+     * @return 切分后的集合
+     */
+    public static List<String> splitTrim(String str, char separator) {
+        return split(str, separator, Boolean.TRUE, Boolean.FALSE);
     }
 
 
     /**
-     * 切分字符串
+     * 根据给定的字符，切分字符串，过滤掉空白元素
+     *
+     * @param str       被切分的字符串
+     * @param separator 分隔符字符
+     * @return 切分后的集合
+     */
+    public static List<String> splitIgnoreEmpty(String str, char separator) {
+        return split(str, separator, Boolean.FALSE, Boolean.TRUE);
+    }
+
+    /**
+     * 根据给定的字符，切分字符串，并去除元素两边空格，并过滤空白符
+     *
+     * @param str       被切分的字符串
+     * @param separator 分隔符字符
+     * @return 切分后的集合
+     */
+    public static List<String> splitTrimAndIgnoreEmpty(String str, char separator) {
+        return split(str, separator, Boolean.TRUE, Boolean.TRUE);
+    }
+
+    /**
+     * 根据给定的字符，切分字符串，忽略大小写
+     *
+     * @param str       被切分的字符串
+     * @param separator 分隔符字符
+     * @return 切分后的集合
+     */
+    public static List<String> splitIgnoreCase(String str, char separator) {
+        return split(str, separator, NONE_LIMIT_INT, Boolean.FALSE, Boolean.FALSE, Boolean.TRUE);
+    }
+
+    /**
+     * 根据给定的字符，切分字符串，大小写敏感，不限制分片
      *
      * @param str         被切分的字符串
      * @param separator   分隔符字符
@@ -100,54 +136,25 @@ public class StringSpliter {
      * @return 切分后的集合
      */
     public static List<String> split(String str, char separator, boolean isTrim, boolean ignoreEmpty) {
-        return split(str, separator, IntConstants.ZERO, isTrim, ignoreEmpty);
+        return split(str, separator, NONE_LIMIT_INT, isTrim, ignoreEmpty, Boolean.FALSE);
     }
 
-
     /**
-     * 切分字符串，大小写敏感，去除每个元素两边空白符
+     * 根据给定的字符，切分字符串，大小写敏感
      *
      * @param str         被切分的字符串
      * @param separator   分隔符字符
-     * @param limit       限制分片数，-1不限制
+     * @param limit       限制分片数，-1 0 不限制
+     * @param isTrim      是否去除切分字符串后每个元素两边的空格
      * @param ignoreEmpty 是否忽略空串
      * @return 切分后的集合
-     */
-    public static List<String> splitTrim(String str, char separator, int limit, boolean ignoreEmpty) {
-        return split(str, separator, limit, Boolean.TRUE, ignoreEmpty, Boolean.FALSE);
-    }
-
-    /**
-     * 切分字符串，大小写敏感
-     *
-     * @param str         被切分的字符串
-     * @param separator   分隔符字符
-     * @param limit       限制分片数 < 0 不限制
-     * @param isTrim      是否去除切分字符串后每个元素离岸边的空格
-     * @param ignoreEmpty 是否忽略空字符
-     * @return 切分后的 List 集合
      */
     public static List<String> split(String str, char separator, int limit, boolean isTrim, boolean ignoreEmpty) {
         return split(str, separator, limit, isTrim, ignoreEmpty, Boolean.FALSE);
     }
 
     /**
-     * 切分字符串，忽略大小写
-     *
-     * @param str         被切分的字符串
-     * @param separator   分隔符字符
-     * @param limit       限制分片数，-1不限制
-     * @param isTrim      是否去除切分字符串后每个元素两边的空格
-     * @param ignoreEmpty 是否忽略空串
-     * @return 切分后的集合
-     */
-    public static List<String> splitIgnoreCase(String str, char separator, int limit, boolean isTrim, boolean ignoreEmpty) {
-        return split(str, separator, limit, isTrim, ignoreEmpty, Boolean.TRUE);
-    }
-
-
-    /**
-     * 切分字符串
+     * 根据给定的字符，切分字符串
      *
      * @param str         被切分的字符串
      * @param separator   分隔符字符
@@ -192,24 +199,79 @@ public class StringSpliter {
         return addToList(list, str.substring(start, strLength), isTrim, ignoreEmpty);
     }
 
-    /**
-     * 切分字符串为字符串数组
-     *
-     * @param str         被切分的字符串
-     * @param separator   分隔符字符
-     * @param limit       限制分片数
-     * @param isTrim      是否去除切分字符串后每个元素两边的空格
-     * @param ignoreEmpty 是否忽略空串
-     * @return 切分后的集合
-     */
-    public static String[] splitToArray(String str, char separator, int limit, boolean isTrim, boolean ignoreEmpty) {
-        return toArray(split(str, separator, limit, isTrim, ignoreEmpty));
-    }
 
     //-------- Split by String ---------
 
     /**
-     * 切分字符串，不忽略大小写
+     * 根据给定的字符串，切分字符串
+     *
+     * @param str       被切分的字符串
+     * @param separator 分隔符字符串
+     * @return 切分后的集合
+     */
+    public static List<String> split(String str, String separator) {
+        return split(str, separator, NONE_LIMIT_INT, Boolean.FALSE, Boolean.FALSE, Boolean.FALSE);
+    }
+
+    /**
+     * 根据给定的字符串，切分字符串，限制分片
+     *
+     * @param str       被切分的字符串
+     * @param separator 分隔符字符串
+     * @param limit     分片限制数
+     * @return 切分后的集合
+     */
+    public static List<String> splitLimit(String str, String separator, int limit) {
+        return split(str, separator, limit, Boolean.FALSE, Boolean.FALSE);
+    }
+
+    /**
+     * 切分字符串，大小写敏感，去除每个元素两边空白符
+     *
+     * @param str       被切分的字符串
+     * @param separator 分隔符字符串
+     * @return 切分后的集合
+     */
+    public static List<String> splitTrim(String str, String separator) {
+        return split(str, separator, Boolean.TRUE, Boolean.FALSE);
+    }
+
+
+    /**
+     * 根据给定的字符串，切分字符串，过滤掉空白元素
+     *
+     * @param str       被切分的字符串
+     * @param separator 分隔符字符串
+     * @return 切分后的集合
+     */
+    public static List<String> splitIgnoreEmpty(String str, String separator) {
+        return split(str, separator, Boolean.FALSE, Boolean.TRUE);
+    }
+
+    /**
+     * 根据给定的字符串，切分字符串，并去除元素两边空格，并过滤空白符
+     *
+     * @param str       被切分的字符串
+     * @param separator 分隔符字符串
+     * @return 切分后的集合
+     */
+    public static List<String> splitTrimAndIgnoreEmpty(String str, String separator) {
+        return split(str, separator, Boolean.TRUE, Boolean.TRUE);
+    }
+
+    /**
+     * 根据给定的字符串，切分字符串，忽略大小写
+     *
+     * @param str       被切分的字符串
+     * @param separator 分隔符字符串
+     * @return 切分后的集合
+     */
+    public static List<String> splitIgnoreCase(String str, String separator) {
+        return split(str, separator, NONE_LIMIT_INT, Boolean.FALSE, Boolean.FALSE, Boolean.TRUE);
+    }
+
+    /**
+     * 根据给定的字符串，切分字符串，大小写敏感，不限制分片
      *
      * @param str         被切分的字符串
      * @param separator   分隔符字符串
@@ -222,23 +284,11 @@ public class StringSpliter {
     }
 
     /**
-     * 切分字符串，去除每个元素两边的空格，忽略大小写
+     * 根据给定的字符串，切分字符串，大小写敏感
      *
      * @param str         被切分的字符串
      * @param separator   分隔符字符串
-     * @param ignoreEmpty 是否忽略空串
-     * @return 切分后的集合
-     */
-    public static List<String> splitTrim(String str, String separator, boolean ignoreEmpty) {
-        return split(str, separator, Boolean.TRUE, ignoreEmpty);
-    }
-
-    /**
-     * 切分字符串，不忽略大小写
-     *
-     * @param str         被切分的字符串
-     * @param separator   分隔符字符串
-     * @param limit       限制分片数
+     * @param limit       限制分片数，-1 0 不限制
      * @param isTrim      是否去除切分字符串后每个元素两边的空格
      * @param ignoreEmpty 是否忽略空串
      * @return 切分后的集合
@@ -248,51 +298,10 @@ public class StringSpliter {
     }
 
     /**
-     * 切分字符串，去除每个元素两边空格，忽略大小写
-     *
-     * @param str         被切分后的字符串
-     * @param separator   分隔符字符串
-     * @param limit       限制分片数
-     * @param ignoreEmpty 是否忽略空字符串
-     * @return 切分后的集合
-     */
-    public static List<String> splitTrim(String str, String separator, int limit, boolean ignoreEmpty) {
-        return split(str, separator, limit, Boolean.TRUE, ignoreEmpty);
-    }
-
-    /**
-     * 切分字符串，忽略大小写
+     * 根据给定的字符串，切分字符串
      *
      * @param str         被切分的字符串
      * @param separator   分隔符字符串
-     * @param limit       限制分片数
-     * @param isTrim      是否去除切分字符串后每个元素两边的空格
-     * @param ignoreEmpty 是否忽略空串
-     * @return 切分后的集合
-     */
-    public static List<String> splitIgnoreCase(String str, String separator, int limit, boolean isTrim, boolean ignoreEmpty) {
-        return split(str, separator, limit, isTrim, ignoreEmpty, Boolean.TRUE);
-    }
-
-    /**
-     * 切分字符串，去除每个元素两边空格，忽略大小写
-     *
-     * @param str         被切分的字符串
-     * @param separator   分隔符字符串
-     * @param limit       限制分片数
-     * @param ignoreEmpty 是否忽略空串
-     * @return 切分后的集合
-     */
-    public static List<String> splitTrimIgnoreCase(String str, String separator, int limit, boolean ignoreEmpty) {
-        return split(str, separator, limit, Boolean.TRUE, ignoreEmpty, Boolean.TRUE);
-    }
-
-
-    /**
-     * 切分字符串
-     *
-     * @param str         被切分的字符串
-     * @param separator   分隔符字符传
      * @param limit       限制分片数，-1 0 不限制
      * @param isTrim      是否去除切分字符串后每个元素两边的空格
      * @param ignoreEmpty 是否忽略空串
@@ -309,7 +318,7 @@ public class StringSpliter {
         }
 
         if (StringUtils.isEmpty(separator)) {
-            return split(str, limit);
+            return splitByWhitespace(str, limit);
         } else if (separator.length() == 1) {
             return split(str, separator.charAt(0), limit, isTrim, ignoreEmpty, ignoreCase);
         }
@@ -340,21 +349,18 @@ public class StringSpliter {
         return addToList(list, str.substring(start, strLength), isTrim, ignoreEmpty);
     }
 
-    /**
-     * 切分字符串为字符串数组
-     *
-     * @param str         被切分的字符串
-     * @param separator   分隔符字符
-     * @param limit       限制分片数
-     * @param isTrim      是否去除切分字符串后每个元素两边的空格
-     * @param ignoreEmpty 是否忽略空串
-     * @return 切分后的集合
-     */
-    public static String[] splitToArray(String str, String separator, int limit, boolean isTrim, boolean ignoreEmpty) {
-        return toArray(split(str, separator, limit, isTrim, ignoreEmpty));
-    }
-
     //-------- Split by Whitespace ---------
+
+    /**
+     * 使用空白符切分字符串
+     * "a b  cd" => {"a","b","cd"}
+     * 切分后的字符串两边不包含空白符，空串或空白符串并不作为元素之一
+     *
+     * @param str 被切分的字符串
+     */
+    public static List<String> splitByWhitespace(String str) {
+        return splitByWhitespace(str, NONE_LIMIT_INT);
+    }
 
     /**
      * 使用空白符切分字符串
@@ -364,7 +370,7 @@ public class StringSpliter {
      * @param str   被切分的字符串
      * @param limit 限制分片数
      */
-    public static List<String> split(String str, int limit) {
+    public static List<String> splitByWhitespace(String str, int limit) {
         if (StringUtils.isEmpty(str)) {
             return ListUtils.newList(IntConstants.ZERO);
         }
@@ -396,22 +402,11 @@ public class StringSpliter {
         return addToList(list, str.substring(start, strLength), Boolean.TRUE, Boolean.TRUE);
     }
 
-    /**
-     * 切分字符串为字符串数组
-     *
-     * @param str   被切分的字符串
-     * @param limit 限制分片数
-     * @return 切分后的集合
-     * @since 3.0.8
-     */
-    public static String[] splitToArray(String str, int limit) {
-        return toArray(split(str, limit));
-    }
-
 
     //---------------------------------------------------------------------------------------------- Split by regex
 
     /**
+     * TODO
      * 通过正则切分字符串
      *
      * @param str            字符串
@@ -428,6 +423,7 @@ public class StringSpliter {
     }
 
     /**
+     * TODO
      * 通过正则切分字符串
      *
      * @param str              字符串
@@ -447,7 +443,7 @@ public class StringSpliter {
         }
 
         if (null == separatorPattern) {//分隔符为空时按照空白符切分
-            return split(str, limit);
+            return splitByWhitespace(str, limit);
         }
 
         final Matcher matcher = separatorPattern.matcher(str);
@@ -464,21 +460,6 @@ public class StringSpliter {
             }
         }
         return addToList(list, str.substring(start, len), isTrim, ignoreEmpty);
-    }
-
-    /**
-     * 通过正则切分字符串为字符串数组
-     *
-     * @param str              被切分的字符串
-     * @param separatorPattern 分隔符正则{@link Pattern}
-     * @param limit            限制分片数
-     * @param isTrim           是否去除切分字符串后每个元素两边的空格
-     * @param ignoreEmpty      是否忽略空串
-     * @return 切分后的集合
-     * @since 3.0.8
-     */
-    public static String[] splitToArray(String str, Pattern separatorPattern, int limit, boolean isTrim, boolean ignoreEmpty) {
-        return toArray(split(str, separatorPattern, limit, isTrim, ignoreEmpty));
     }
 
     //-------- Split Tool  ---------
@@ -503,14 +484,4 @@ public class StringSpliter {
         return list;
     }
 
-
-    /**
-     * List转Array
-     *
-     * @param list List
-     * @return Array
-     */
-    private static String[] toArray(List<String> list) {
-        return list.toArray(new String[0]);
-    }
 }
--- NiufenCommon/src/test/java/io/niufen/common/text/StringSpliterTest.java
+++ NiufenCommon/src/test/java/io/niufen/common/text/StringSpliterTest.java
@@ -8,19 +8,91 @@ import java.util.List;
 public class StringSpliterTest {
 
     @Test
-    public void split() {
-        String str = "a-b-c-d-e";
+    public void splitPath() {
+        // 切分字符串路径，根据当前系统的默认路径进行切分
+        String path = "/Users/niufen/Desktop/lucky_order_all.sql";
+        List<String> pathList = StringSpliter.splitPath(path);
+
+        assert 4 == pathList.size();
+        assert "Users".equals(pathList.get(0));
+        assert "niufen".equals(pathList.get(1));
+        assert "Desktop".equals(pathList.get(2));
+        assert "lucky_order_all.sql".equals(pathList.get(3));
+
+        pathList = StringSpliter.splitPath(path,1);
+        assert 1 == pathList.size();
+        assert path.equals(pathList.get(0));
+
+        pathList = StringSpliter.splitPath(path,2);
+        assert 2 == pathList.size();
+        assert "Users".equals(pathList.get(0));
+        assert "niufen/Desktop/lucky_order_all.sql".equals(pathList.get(1));
+
+        pathList = StringSpliter.splitPath(path,3);
+        assert 3 == pathList.size();
+        assert "Users".equals(pathList.get(0));
+        assert "niufen".equals(pathList.get(1));
+        assert "Desktop/lucky_order_all.sql".equals(pathList.get(2));
+    }
+
+    @Test
+    public void splitByChar() {
+
+        // 切分字符串，根据给定的字符
+        String str = "a-b-c ";
         char separator = '-';
-        List<String> splitList = StringSpliter.split(str, separator, -1, false, false, false);
-        assert 5 == splitList.size();
+        List<String> splitList = StringSpliter.split(str, separator);
+        assert 3 == splitList.size();
+        assert "a".equals(splitList.get(0));
+        assert "b".equals(splitList.get(1));
+        assert "c ".equals(splitList.get(2));
+
+        // 根据给定的字符，切分字符串，限制分片
+        splitList = StringSpliter.splitLimit(str, separator,2);
+        assert 2 == splitList.size();
+        assert "a".equals(splitList.get(0));
+        assert "b-c ".equals(splitList.get(1));
+
+        // 切分字符串，大小写敏感，去除每个元素两边空白符
+        str = "a-- b-c ";
+        separator = '-';
+        splitList = StringSpliter.splitTrim(str,separator);
+        assert 4 == splitList.size();
+        assert "a".equals(splitList.get(0));
+        assert "".equals(splitList.get(1));
+        assert "b".equals(splitList.get(2));
+        assert "c".equals(splitList.get(3));
+
+        // 根据给定的字符，切分字符串，过滤掉空白元素
+        str = "a-- b-c ";
+        separator = '-';
+        splitList = StringSpliter.splitIgnoreEmpty(str,separator);
+        assert 3 == splitList.size();
+        assert "a".equals(splitList.get(0));
+        assert " b".equals(splitList.get(1));
+        assert "c ".equals(splitList.get(2));
+
+        // 根据给定的字符，切分字符串，并去除元素两边空格，并过滤空白符
+        str = "a- - b-c ";
+        separator = '-';
+        splitList = StringSpliter.splitTrimAndIgnoreEmpty(str,separator);
+        assert 3 == splitList.size();
         assert "a".equals(splitList.get(0));
         assert "b".equals(splitList.get(1));
         assert "c".equals(splitList.get(2));
-        assert "d".equals(splitList.get(3));
-        assert "e".equals(splitList.get(4));
+
+        // 根据给定的字符，切分字符串，忽略大小写
+        str = "1A2a3";
+        separator = 'a';
+        splitList = StringSpliter.splitIgnoreCase(str,separator);
+        assert 3 == splitList.size();
+        assert "1".equals(splitList.get(0));
+        assert "2".equals(splitList.get(1));
+        assert "3".equals(splitList.get(2));
 
         str = "a-b-c-d-e";
-        splitList = StringSpliter.split(str, separator, 0, false, false, false);
+        separator = '-';
+        splitList = StringSpliter.split(str, separator, -1, false, false, false);
         assert 5 == splitList.size();
         assert "a".equals(splitList.get(0));
         assert "b".equals(splitList.get(1));
@@ -38,28 +110,6 @@ public class StringSpliterTest {
         assert 2 == splitList.size();
         assert "a".equals(splitList.get(0));
         assert "b-c-d-e".equals(splitList.get(1));
-
-        str = "a-b-c-d-e";
-        splitList = StringSpliter.split(str, separator, 3, false, false, false);
-        assert 3 == splitList.size();
-        assert "a".equals(splitList.get(0));
-        assert "b".equals(splitList.get(1));
-        assert "c-d-e".equals(splitList.get(2));
-
-        str = "a-b- -d-e";
-        splitList = StringSpliter.split(str, separator, 5, false, false, false);
-        assert 5 == splitList.size();
-        assert "a".equals(splitList.get(0));
-        assert "b".equals(splitList.get(1));
-        assert " ".equals(splitList.get(2));
-
-        str = "a-b- -d-e";
-        splitList = StringSpliter.split(str, separator, 5, true, false, false);
-        assert 5 == splitList.size();
-        assert "a".equals(splitList.get(0));
-        assert "b".equals(splitList.get(1));
-        assert "".equals(splitList.get(2));
-
         str = "a-b- -d-e";
         splitList = StringSpliter.split(str, separator, 5, true, true, false);
         assert 4 == splitList.size();
@@ -69,12 +119,110 @@ public class StringSpliterTest {
     }
 
     @Test
-    public void splitWhitespace(){
-        String str = "a b"+ StringConstants.Mark.SPACE_CN + StringConstants.Mark.SPACE_CN +"c"+""+"d";
-        List<String> stringList = StringSpliter.split(str, 0);
+    public void splitByString() {
+
+        // 切分字符串，根据给定的字符串
+        String str = "a-b-c ";
+        String separator = "-";
+        List<String> splitList = StringSpliter.split(str, separator);
+        assert 3 == splitList.size();
+        assert "a".equals(splitList.get(0));
+        assert "b".equals(splitList.get(1));
+        assert "c ".equals(splitList.get(2));
+
+        str = "a||b||c ";
+        separator = "||";
+        splitList = StringSpliter.split(str, separator);
+        assert 3 == splitList.size();
+        assert "a".equals(splitList.get(0));
+        assert "b".equals(splitList.get(1));
+        assert "c ".equals(splitList.get(2));
+
+        // 根据给定的字符串，切分字符串，限制分片
+        str = "a-b-c ";
+        separator = "-";
+        splitList = StringSpliter.splitLimit(str, separator,2);
+        assert 2 == splitList.size();
+        assert "a".equals(splitList.get(0));
+        assert "b-c ".equals(splitList.get(1));
+
+        // 切分字符串，大小写敏感，去除每个元素两边空白符
+        str = "a-- b-c ";
+        separator = "-";
+        splitList = StringSpliter.splitTrim(str,separator);
+        assert 4 == splitList.size();
+        assert "a".equals(splitList.get(0));
+        assert "".equals(splitList.get(1));
+        assert "b".equals(splitList.get(2));
+        assert "c".equals(splitList.get(3));
+
+        // 根据给定的字符串，切分字符串，过滤掉空白元素
+        str = "a-- b-c ";
+        separator = "-";
+        splitList = StringSpliter.splitIgnoreEmpty(str,separator);
+        assert 3 == splitList.size();
+        assert "a".equals(splitList.get(0));
+        assert " b".equals(splitList.get(1));
+        assert "c ".equals(splitList.get(2));
+
+        // 根据给定的字符串，切分字符串，并去除元素两边空格，并过滤空白符
+        str = "a- - b-c ";
+        separator = "-";
+        splitList = StringSpliter.splitTrimAndIgnoreEmpty(str,separator);
+        assert 3 == splitList.size();
+        assert "a".equals(splitList.get(0));
+        assert "b".equals(splitList.get(1));
+        assert "c".equals(splitList.get(2));
+
+        // 根据给定的字符串，切分字符串，忽略大小写
+        str = "1A2a3";
+        separator = "a";
+        splitList = StringSpliter.splitIgnoreCase(str,separator);
+        assert 3 == splitList.size();
+        assert "1".equals(splitList.get(0));
+        assert "2".equals(splitList.get(1));
+        assert "3".equals(splitList.get(2));
+
+        str = "a-b-c-d-e";
+        separator = "-";
+        splitList = StringSpliter.split(str, separator, -1, false, false, false);
+        assert 5 == splitList.size();
+        assert "a".equals(splitList.get(0));
+        assert "b".equals(splitList.get(1));
+        assert "c".equals(splitList.get(2));
+        assert "d".equals(splitList.get(3));
+        assert "e".equals(splitList.get(4));
+
+        str = "a-b-c-d-e";
+        splitList = StringSpliter.split(str, separator, 1, false, false, false);
+        assert 1 == splitList.size();
+        assert str.equals(splitList.get(0));
+
+        str = "a-b-c-d-e";
+        splitList = StringSpliter.split(str, separator, 2, false, false, false);
+        assert 2 == splitList.size();
+        assert "a".equals(splitList.get(0));
+        assert "b-c-d-e".equals(splitList.get(1));
+        str = "a-b- -d-e";
+        splitList = StringSpliter.split(str, separator, 5, true, true, false);
+        assert 4 == splitList.size();
+        assert "a".equals(splitList.get(0));
+        assert "b".equals(splitList.get(1));
+        assert "d".equals(splitList.get(2));
+    }
+
+    @Test
+    public void splitWhitespace() {
+        String str = "a b" + StringConstants.Mark.SPACE_CN + StringConstants.Mark.SPACE_CN + "c" + "" + "d";
+        List<String> stringList = StringSpliter.splitByWhitespace(str);
         assert 3 == stringList.size();
         assert "a".equals(stringList.get(0));
         assert "b".equals(stringList.get(1));
         assert "cd".equals(stringList.get(2));
+
+        stringList = StringSpliter.splitByWhitespace(str,2);
+        assert 2 == stringList.size();
+        assert "a".equals(stringList.get(0));
+        assert str.substring(2).equals(stringList.get(1));
     }
 }
